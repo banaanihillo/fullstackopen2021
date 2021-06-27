08a41153559d34d7be8f29b9000586ceb76d22d4
banaanihillo <banaanihillo@protonmail.ch>
2021-06-27 14:59:38 +0300

    1-04: Moved calculation of exercises into Total
    Refactored the Application component,
    into passing the parts array directly,
    and then calculating the number of exercises

--- exercise1/course-information/src/components/Total.vue
+++ exercise1/course-information/src/components/Total.vue
@@ -5,7 +5,14 @@
 <script>
 export default {
   props: {
-    numberOfExercises: Number
+    parts: Array
+  },
+  computed: {
+    numberOfExercises() {
+      return Object.values(this.parts).reduce((accumulator, part) => {
+        return (accumulator + part.exercises)
+      }, 0)
+    }
   }
 }
 </script>
