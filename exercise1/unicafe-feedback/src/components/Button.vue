d552b3243a2c7073ed8dc6e086ea2371b1e85bfd
banaanihillo <banaanihillo@protonmail.ch>
2021-06-28 13:06:51 +0300

    1-10: Separated buttons into Buttons
    Refactored actual buttons into "buttons",
    taking the label as a property,
    and trickling down the feedback state as well

---
+++ exercise1/unicafe-feedback/src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="increment(label.toLowerCase())">
+    {{label}}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    feedback: Object
+  },
+  methods: {
+    increment(feedbackType) {
+      return this.feedback[feedbackType]++
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
