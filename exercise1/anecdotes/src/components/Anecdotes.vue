ea3d7d58945fc1d0af2673d074535090bb7ea971
banaanihillo <banaanihillo@protonmail.ch>
2021-06-28 15:26:00 +0300

    1-13: Added anecdote votes
    Refactored the anecdotes into an array of objects,
    each with the anecdote content and votes;
    Added a button with which to vote for an anecdote

--- exercise1/anecdotes/src/components/Anecdotes.vue
+++ exercise1/anecdotes/src/components/Anecdotes.vue
@@ -1,8 +1,15 @@
 <template>
   <span>
     <p>
-      {{anecdotes[currentIndex]}}
+      {{anecdotes[currentIndex].content}}
     </p>
+    <p>
+      Votes for this anecdote: {{anecdotes[currentIndex].votes}}
+    </p>
+    <button @click="vote(currentIndex)" class="positive">
+      Vote
+    </button>
+    <br />
     <button @click="generateRandomIndex">
       New anecdote
     </button>
@@ -14,19 +21,42 @@ export default {
   data() {
     return {
       anecdotes: [
-        "If it hurts, do it more often",
-        "Adding [developers] to a late software project"
-        + " makes it later!",
-        "The first 90 percent of the code accounts for the first"
-        + " 90 percent of the development time."
-        + " The remaining[omitted]",
-        "Any fool can write code that a computer can understand."
-        + " Good programmers write code that humans can und[omitted]",
-        "Premature optimization is the root of all evil.",
-        "Debugging is twice as hard as writing the code in the first"
-        + " place. Therefore, if you write the code as[omitted]",
-        "Programming without an extremely heavy use of console.log"
-        + " is something something with grammatical errors"
+        {
+          content: "If it hurts, do it more often",
+          votes: 0
+        },
+        {
+          content: "Adding [developers] to a late software project"
+            + " makes it later!",
+          votes: 0
+        },
+        {
+          content: "The first 90 percent of the code accounts for"
+            + " the first 90 percent of the development time."
+            + " The remaining[omitted]",
+          votes: 0
+        },
+        {
+          content: "Any fool can write code"
+            + " that a computer can understand. Good"
+            + " programmers write code that humans can und[omitted]",
+          votes: 0
+        },
+        {
+          content: "Premature optimization is the root of all evil.",
+          votes: 0
+        },
+        {
+          content: "Debugging is twice as hard as writing the code"
+            + " in the first place."
+            + " Therefore, if you write the code as[omitted]",
+          votes: 0
+        },
+        {
+          content: "Programming without an extremely heavy use of"
+            + " console.log is something something grammatical errors",
+          votes: 0
+        }
       ],
       currentIndex: 0
     }
@@ -37,11 +67,20 @@ export default {
         Math.random() * this.anecdotes.length
       )
       return this.currentIndex = randomNumber
+    },
+    vote(index) {
+      this.anecdotes[index].votes++
     }
   }
 }
 </script>
 
 <style scoped>
+button {
+  margin-bottom: 1em;
+}
 
+.positive {
+  background-color: lightgreen;
+}
 </style>
