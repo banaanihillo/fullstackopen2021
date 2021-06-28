5f9d8f741fa5a1f14521fe1f29a92f8c109456dd
banaanihillo <banaanihillo@protonmail.ch>
2021-06-28 11:59:49 +0300

    1-07: Added feedback average and positive %
    Added computed properties for feedback scores,
    including the total of feedback given,
    and a weighted score (+1, 0, -1) accordingly,
    to enable calculation of the average score,
    and the percentage of positive feedback given

---
+++ exercise1/unicafe-feedback/src/components/Statistics.vue
@@ -0,0 +1,80 @@
+<template>
+<span>
+  <h2> Statistics </h2>
+  <p>
+    Positive: {{feedback.positive}}
+  </p>
+  <p>
+    Neutral: {{feedback.neutral}}
+  </p>
+  <p>
+    Negative: {{feedback.negative}}
+  </p>
+  <p>
+    Total: {{totalFeedback}}
+  </p>
+  <p>
+    Average: {{average}}
+  </p>
+  <p>
+    Positive: {{positivePercentage}}
+  </p>
+</span>
+</template>
+
+<script>
+export default {
+  props: {
+    feedback: Object
+  },
+  methods: {
+    calculateSum(things) {
+      return things.reduce((accumulator, currentValue) => {
+        return (accumulator + currentValue)
+      }, 0)
+    }
+  },
+  computed: {
+    totalFeedback() {
+      return this.calculateSum(Object.values(this.feedback))
+    },
+    average() {
+      // Division by zero
+      if (this.totalFeedback === 0) {
+        return 0
+      }
+      return (
+        this.calculateSum(this.scores)
+        / this.totalFeedback
+      )
+    },
+    scores() {
+      return Object.entries(this.feedback).map(
+        ([feedbackType, score]) => {
+          switch (feedbackType) {
+            case "positive":
+              return 1 * score
+            case "neutral":
+              return 0
+            case "negative":
+              return -1 * score
+            default:
+              throw new Error("How did you get here")
+          }
+        }
+      )
+    },
+    positivePercentage() {
+      return (
+        (this.feedback.positive
+        / this.totalFeedback)
+        * 100
+      )
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
