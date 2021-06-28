4527ee61f3d675cfb18267e3ba65522424ec3f25
banaanihillo <banaanihillo@protonmail.ch>
2021-06-28 14:58:59 +0300

    1-12: Added random anecdotes
    Added hard-coded anecdotes,
    which are supposedly one-liners,
    but n-liners in practice,
    so had to omit the punchline in most of them,
    also added a button for new index generation,
    to essentially fetch a random anecdote

---
+++ exercise1/anecdotes/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Anecdotes />
+</template>
+
+<script>
+import Anecdotes from "./components/Anecdotes"
+
+export default {
+  name: 'App',
+  components: {
+    Anecdotes
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  background-color: black;
+  color: magenta;
+  margin-top: 60px;
+}
+
+button {
+  background-color: plum;
+}
+</style>
---
+++ exercise1/anecdotes/src/components/Anecdotes.vue
@@ -0,0 +1,47 @@
+<template>
+  <span>
+    <p>
+      {{anecdotes[currentIndex]}}
+    </p>
+    <button @click="generateRandomIndex">
+      New anecdote
+    </button>
+  </span>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      anecdotes: [
+        "If it hurts, do it more often",
+        "Adding [developers] to a late software project"
+        + " makes it later!",
+        "The first 90 percent of the code accounts for the first"
+        + " 90 percent of the development time."
+        + " The remaining[omitted]",
+        "Any fool can write code that a computer can understand."
+        + " Good programmers write code that humans can und[omitted]",
+        "Premature optimization is the root of all evil.",
+        "Debugging is twice as hard as writing the code in the first"
+        + " place. Therefore, if you write the code as[omitted]",
+        "Programming without an extremely heavy use of console.log"
+        + " is something something with grammatical errors"
+      ],
+      currentIndex: 0
+    }
+  },
+  methods: {
+    generateRandomIndex() {
+      const randomNumber = Math.floor(
+        Math.random() * this.anecdotes.length
+      )
+      return this.currentIndex = randomNumber
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
