55aa07611ca86a35d73890fd4a2662bc88de2fb7
banaanihillo <banaanihillo@protonmail.ch>
2021-06-29 17:41:24 +0300

    2-06: Added phone book with name submission
    Added people with no numbers yet,
    also added emission from the phone book,
    back into the parent application data property,
    to concatenate the new person into the people

---
+++ exercise2/phonebook/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<span>
+  <Phonebook :people="people" @add-person="addPerson" />
+</span>
+</template>
+
+<script>
+import Phonebook from './components/Phonebook.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Phonebook
+  },
+  data() {
+    return {
+      people: [
+        {
+          name: "Banana Hillo"
+        },
+        {
+          name: "Hillo Banana"
+        }
+      ]
+    }
+  },
+  methods: {
+    addPerson(input) {
+      this.people = [
+        ...this.people,
+        {
+          ...input
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  background-color: black;
+  color: magenta;
+  margin-top: 60px;
+}
+</style>
---
+++ exercise2/phonebook/src/components/Phonebook.vue
@@ -0,0 +1,56 @@
+<template>
+<span>
+  <h1> Names but no numbers </h1>
+  <h2> Names </h2>
+  <ul>
+    <li v-for="person in people" :key="person.name">
+      {{person.name}}
+    </li>
+  </ul>
+  <h2> Add a new name </h2>
+  <form @submit.prevent="onSubmit">
+    <label>
+      Name
+      <input
+        type="text"
+        v-model="input.name"
+      />
+    </label>
+    <br />
+    <button type="submit">
+      Add
+    </button>
+  </form>
+</span>
+</template>
+
+<script>
+export default {
+  props: {
+    people: Array
+  },
+  data() {
+    return {
+      input: {
+        name: ""
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("add-person", this.input)
+    }
+  }
+}
+</script>
+
+<style scoped>
+input {
+  background-color: violet;
+}
+
+button {
+  background-color: plum;
+  margin-top: 1em;
+}
+</style>
