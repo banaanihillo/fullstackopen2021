9cbf2e04abd9c08652fe4737244b3d5c7be67e54
banaanihillo <banaanihillo@protonmail.ch>
2021-06-30 12:32:40 +0300

    2-10: Removed the logic from the phone book
    Moved the filter and submission up to the root,
    which now handles all the necessary logic,
    leaving the phone book component presentational

--- exercise2/phonebook/src/components/Phonebook.vue
+++ exercise2/phonebook/src/components/Phonebook.vue
@@ -1,102 +1,36 @@
 <template>
 <span>
-  <h1> Phone book </h1>
-  <label for="search"> Filter by name </label>
-  <input
-    type="search"
-    id="search"
-    v-model="search"
-  />
-
-  <h2> Numbers </h2>
+  <h2> Names and numbers </h2>
   <ul>
     <li v-for="person in filteredPeople" :key="person.name">
       {{person.name}} ({{person.number}})
     </li>
   </ul>
 
-  <h2> Add a new name </h2>
-  <form @submit.prevent="onSubmit">
-    <span class="input">
-      <label for="name-input"> Name </label>
-      <input
-        type="text"
-        v-model="input.name"
-        id="name-input"
-      />
-    </span>
-    <br />
-    <span class="input">
-      <label for="number-input"> Number </label>
-      <input
-        type="text"
-        v-model="input.number"
-      />
-    </span>
-    <br />
-    <button type="submit">
-      Add
-    </button>
-  </form>
+
 </span>
 </template>
 
 <script>
+
 export default {
   props: {
-    people: Array
+    filteredPeople: Array
   },
   data() {
     return {
-      input: {
-        name: "",
-        number: ""
-      },
-      search: ""
+      
     }
   },
   methods: {
-    onSubmit() {
-      this.$emit("add-person", this.input)
-      this.input = {
-        name: "",
-        number: ""
-      }
-    }
+
   },
   computed: {
-    filteredPeople() {
-      if (this.search) {
-        return this.people.filter((person) => {
-          return person.name.toLowerCase().includes(
-            this.search.toLowerCase()
-          )
-        })
-      } else {
-        return this.people
-      }
-    }
+    
   }
 }
 </script>
 
-<style scoped>
-input {
-  background-color: violet;
-}
-
-button {
-  background-color: plum;
-  margin-top: 1em;
-}
-
-.input {
-  display: flex;
-  justify-content: center;
-}
-
-.input label {
-  width: 5em;
-}
+<style>
 
 </style>
