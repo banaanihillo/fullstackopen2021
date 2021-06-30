a9f7a174a96fe22ed620a5c39776607595220a8a
banaanihillo <banaanihillo@protonmail.ch>
2021-06-30 20:20:54 +0300

    2-14: Added weather information
    Added various weather data for the country capital,
    including temperature, wind and precipitation,
    and the observation date in local time

--- exercise2/rest-countries/src/components/CountryInformation.vue
+++ exercise2/rest-countries/src/components/CountryInformation.vue
@@ -22,6 +22,15 @@
     :alt="'Flag of ' + country.name"
     width="100"
   />
+
+  <h4> Weather in {{country.capital}} </h4>
+  <p v-if="weather">
+    Temperature: {{weather.current.temp_c + "\u00B0"}} <br />
+    Wind: {{weather.current.wind_kph}} km/h
+    (Gusts up to {{weather.current.gust_kph}} km/h) <br />
+    Precipitation: {{weather.current.precip_mm}} mm <br />
+    Last updated at {{weather.current.last_updated}} (Local)
+  </p>
 </span>
 </template>
 
@@ -29,6 +38,23 @@
 export default {
   props: {
     country: Object
+  },
+  data() {
+    return {
+      weather: null
+    }
+  },
+  async created() {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${
+        process.env.VUE_APP_WEATHER_API_KEY
+      }&q=${this.country.capital}`
+    )
+    if (!response.ok) {
+      throw new Error("Response not OK")
+    }
+    const weather = await response.json()
+    this.weather = weather
   }
 }
 </script>
