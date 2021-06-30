6bad16a59edb5edecb20cb57f78ad686f60d748c
banaanihillo <banaanihillo@protonmail.ch>
2021-06-30 12:28:36 +0300

    2-10: Moved submission into a new component
    Added a person form submission component,
    with emission back to the application root on submit

---
+++ exercise2/phonebook/src/components/AddPerson.vue
@@ -0,0 +1,53 @@
+<template>
+<span>
+  <h2> Add a new name </h2>
+  <form @submit.prevent="onSubmit">
+    <span class="input">
+      <label for="name-input"> Name </label>
+      <input
+        type="text"
+        v-model="input.name"
+        id="name-input"
+      />
+    </span>
+    <br />
+    <span class="input">
+      <label for="number-input"> Number </label>
+      <input
+        type="text"
+        v-model="input.number"
+      />
+    </span>
+    <br />
+    <button type="submit">
+      Add
+    </button>
+  </form>
+</span>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: {
+        name: "",
+        number: ""
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("add-person", this.input)
+      this.input = {
+        name: "",
+        number: ""
+      }
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
