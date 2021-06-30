60dc902bb4b30bf9ae8ca62bad66cd24bd9ebaf3
banaanihillo <banaanihillo@protonmail.ch>
2021-06-30 19:15:31 +0300

    2-13: Refactored extended country information
    Moved the extra information into a new component,
    to simplify the syntax on search query match,
    and to allow for expansion from the list of results

---
+++ exercise2/rest-countries/src/components/CountryInformation.vue
@@ -0,0 +1,38 @@
+<template>
+<span>
+  <h2> {{country.name}} </h2>
+  <p>
+    Native name: {{country.nativeName}} <br />
+    Capital: {{country.capital}} <br />
+    Population: {{country.population}} <br />
+  </p>
+
+  <h3> Languages </h3>
+  <ul>
+    <li
+      v-for="language in country.languages"
+      :key="language.iso639_2"
+    >
+      {{language.name}} ({{language.nativeName}})
+    </li>
+  </ul>
+
+  <img
+    :src="country.flag"
+    :alt="'Flag of ' + country.name"
+    width="100"
+  />
+</span>
+</template>
+
+<script>
+export default {
+  props: {
+    country: Object
+  }
+}
+</script>
+
+<style>
+
+</style>
