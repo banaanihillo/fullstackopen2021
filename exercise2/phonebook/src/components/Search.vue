d01c301611dcd2ffe99d4f549043e68faeb4f5f7
banaanihillo <banaanihillo@protonmail.ch>
2021-06-30 12:30:21 +0300

    2-10: Separated the search functionality
    Added a new component for the name search,
    which emits its search query to the application root

---
+++ exercise2/phonebook/src/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+<span>
+  <label for="search"> Filter by name </label>
+  <input
+    type="search"
+    id="search"
+    v-model="searchQuery"
+    @input="onSearch"
+  />
+</span>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: ""
+    }
+  },
+  methods: {
+    onSearch() {
+      this.$emit("search-people", this.searchQuery)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
