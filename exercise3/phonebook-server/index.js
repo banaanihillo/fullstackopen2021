976b457b4aa02268dec7b2525ab84ac84045d850
banaanihillo <banaanihillo@protonmail.ch>
2021-07-03 15:28:08 +0300

    3-02: Added phone book information
    Added a rudimentary html string,
    containing the number of people in the phone book,
    and the observation date

--- exercise3/phonebook-server/index.js
+++ exercise3/phonebook-server/index.js
@@ -7,16 +7,29 @@ const dummyPeople = [
     id: "ahahahhaahghahahahahhhhhhhhhhaaaaaaaaa",
     name: "Banana Hillo",
     number: "1-800-BHLL"
-  },
+  }/* ,
   {
     id: "a7a7a7a7",
     name: "Hillo Banaan",
     number: "6-HLL-BBNN"
-  }
+  } */
 ]
 
 app.get("/api/people", (_request, response) => {
   response.send(dummyPeople)
+})
+
+app.get("/info", (_request, response) => {
+  response.send(`
+    <p> The phonebook contains ${dummyPeople.length} ${
+      dummyPeople.length === 1
+        ? "person"
+        : "people"
+    }. </p>
+    <p>
+      Observation date: ${new Date().toISOString()}
+    </p>
+  `)
 })
 
 const PORT = process.env.PORT || 3001
