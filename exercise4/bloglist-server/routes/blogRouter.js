604bc2a47f29385e28e99600ef5ed3c8e4ab234a
banaanihillo <banaanihillo@protonmail.ch>
2021-07-08 16:17:54 +0300

    4-08+: Added GET and DELETE by ID

--- exercise4/bloglist-server/routes/blogRouter.js
+++ exercise4/bloglist-server/routes/blogRouter.js
@@ -7,9 +7,43 @@ blogRouter.get("/", async (_request, response) => {
 })
 
 blogRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body)
-  const newBlog = await blog.save()
-  response.status(201).json(newBlog)
+  try {
+    const blog = new Blog(request.body)
+    const newBlog = await blog.save()
+    response.status(201).json(newBlog)
+  } catch (exception) {
+    console.error(exception)
+    return response.status(400).json({
+      error: `Title and author are required. ${exception}`
+    })
+  }
+})
+
+blogRouter.get("/:id", async (request, response) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (!blog) {
+      return response.status(404).json({
+        error: `No blog found with the ID ${request.params.id}.`
+      })
+    }
+    response.json(blog)
+  } catch (exception) {
+    console.error("Something else went wrong", exception)
+    throw new Error(exception)
+  }
+})
+
+blogRouter.delete("/:id", async (request, response) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    console.log("Nothing to remove? Have a 204 anyway")
+    response.status(204).send({
+      error: `${request.params.id} did not exist. ${exception}`
+    })
+  }
 })
 
 module.exports = blogRouter
