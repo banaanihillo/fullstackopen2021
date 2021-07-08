125b8079ffdb1bc14ad8ee8832f5f5cf2485dcfa
banaanihillo <banaanihillo@protonmail.ch>
2021-07-08 16:16:41 +0300

    4-08+: Added validation for blogs
    Made the blog author and title fields required

--- exercise4/bloglist-server/models/Blog.js
+++ exercise4/bloglist-server/models/Blog.js
@@ -1,10 +1,31 @@
 const mongoose = require("mongoose")
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
   url: String,
   upvotes: Number
+})
+blogSchema.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    // _id is an ObjectID Object by default,
+    // this thing returns that same thing, but as a String,
+    // essentially stripping the _bsontype property,
+    // and stringifying the UInt8Array element;
+    // also rename the property from _id to id,
+    // so it looks less like an internal property, I guess?
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject.__v
+    // also remove that unrenamed property,
+    // since that thing is now in the id property
+    delete returnedObject._id
+  }
 })
 
 module.exports = mongoose.model("Blog", blogSchema)
