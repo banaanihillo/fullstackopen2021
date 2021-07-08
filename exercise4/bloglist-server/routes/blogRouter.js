222b43172c50d3421fdb35745c49d9cfd2bae2b2
banaanihillo <banaanihillo@protonmail.ch>
2021-07-08 18:56:29 +0300

    Removed the manual error handling
    Delegated exceptions to the error handler

--- exercise4/bloglist-server/routes/blogRouter.js
+++ exercise4/bloglist-server/routes/blogRouter.js
@@ -7,66 +7,49 @@ blogRouter.get("/", async (_request, response) => {
 })
 
 blogRouter.post("/", async (request, response) => {
-  try {
-    const blogProperties = {
-      ...request.body,
-      upvotes: request.body.upvotes || 0
-    }
-    const blog = new Blog(blogProperties)
-    const newBlog = await blog.save()
-    response.status(201).json(newBlog)
-  } catch (exception) {
 
-    return response.status(400).json({
-      error: `Title and author are required. ${exception}`
-    })
+  const blogProperties = {
+    ...request.body,
+    upvotes: request.body.upvotes || 0
   }
+  const blog = new Blog(blogProperties)
+  const newBlog = await blog.save()
+  response.status(201).json(newBlog)
+  //
 })
 
 blogRouter.get("/:id", async (request, response) => {
-  try {
-    const blog = await Blog.findById(request.params.id)
-    if (!blog) {
-      return response.status(404).json({
-        error: `No blog found with the ID ${request.params.id}.`
-      })
-    }
-    response.json(blog)
-  } catch (exception) {
-    console.error("Something else went wrong", exception)
-    throw new Error(exception)
+
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).json({
+      error: `No blog found with the ID ${request.params.id}.`
+    })
   }
+  response.json(blog)
+
 })
 
 blogRouter.delete("/:id", async (request, response) => {
-  try {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  } catch (exception) {
-    console.log("Nothing to remove? Have a 204 anyway")
-    response.status(204).send({
-      error: `${request.params.id} did not exist. ${exception}`
-    })
-  }
+
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+
 })
 
 blogRouter.patch("/:id", async (request, response) => {
-  try {
-    const updatedBlog = await Blog.findByIdAndUpdate(
-      request.params.id,
-      {
-        upvotes: request.body.upvotes
-      },
-      {
-        new: true
-      }
-    )
-    response.status(201).json(updatedBlog)
-  } catch (exception) {
-    response.status(404).json({
-      error: `${request.params.id} not found. ${exception}`
-    })
-  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    {
+      upvotes: request.body.upvotes
+    },
+    {
+      new: true
+    }
+  )
+  response.status(201).json(updatedBlog)
+  
 })
 
 module.exports = blogRouter
