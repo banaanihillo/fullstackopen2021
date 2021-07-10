135f158a2a885a9177c1650a4e6a98f512f385b1
banaanihillo <banaanihillo@protonmail.ch>
2021-07-10 12:34:38 +0300

    4-15 to 4-16: Added token errors

--- exercise4/bloglist-server/app.js
+++ exercise4/bloglist-server/app.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 require("express-async-errors")
 const blogRouter = require("./routes/blogRouter")
 const userRouter = require("./routes/userRouter")
+const loginRouter = require("./routes/loginRouter")
 
 require("dotenv").config()
 const databaseAddress = (
@@ -30,6 +31,7 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/blogs", blogRouter)
 app.use("/api/users", userRouter)
+app.use("/api/login", loginRouter)
 
 const notFound = (request, response) => {
   response.status(404).send({
@@ -40,15 +42,26 @@ app.use(notFound)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
-  if (error.name === "CastError") {
-    response.status(400).send({
-      error: `Malformed ID ${request.params.id}. ${error}`
-    })
-  } else if (error.name === "ValidationError") {
-    response.status(400).send({
-      error: error.message
-    })
+
+  switch (error.name) {
+    case "CastError":
+      return response.status(400).send({
+        error: `Malformed ID ${request.params.id}. ${error}`
+      })
+    case "ValidationError":
+      return response.status(400).send({
+        error: error.message
+      })
+    case "JsonWebTokenError":
+      return response.status(401).send({
+        error: `Invalid token. ${error.message}`
+      })
+    case "TokenExpiredError":
+      return response.status(401).send({
+        error: `Token expired. ${error.message}`
+      })
   }
+  
   next(error)
 }
 app.use(errorHandler)
