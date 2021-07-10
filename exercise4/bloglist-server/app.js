ea3a51309782688a881f27f55e4f930b994489e9
banaanihillo <banaanihillo@protonmail.ch>
2021-07-10 13:33:02 +0300

    4-17 to 4-20: Moved middle ware into a module
    Refactored the custom middle ware functions,
    into a new module

--- exercise4/bloglist-server/app.js
+++ exercise4/bloglist-server/app.js
@@ -2,6 +2,11 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 require("express-async-errors")
+const {
+  tokenExtractor,
+  notFound,
+  errorHandler
+} = require("./customMiddleWare")
 const blogRouter = require("./routes/blogRouter")
 const userRouter = require("./routes/userRouter")
 const loginRouter = require("./routes/loginRouter")
@@ -29,42 +34,12 @@ mongoose.connect(
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use(tokenExtractor)
 app.use("/api/blogs", blogRouter)
 app.use("/api/users", userRouter)
 app.use("/api/login", loginRouter)
-
-const notFound = (request, response) => {
-  response.status(404).send({
-    error: `No such address found: ${request.url}`
-  })
-}
-app.use(notFound)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  switch (error.name) {
-    case "CastError":
-      return response.status(400).send({
-        error: `Malformed ID ${request.params.id}. ${error}`
-      })
-    case "ValidationError":
-      return response.status(400).send({
-        error: error.message
-      })
-    case "JsonWebTokenError":
-      return response.status(401).send({
-        error: `Invalid token. ${error.message}`
-      })
-    case "TokenExpiredError":
-      return response.status(401).send({
-        error: `Token expired. ${error.message}`
-      })
-  }
-  
-  next(error)
-}
-app.use(errorHandler)
-
+app.use(notFound) //
+app.use(errorHandler) //
 
 module.exports = app
