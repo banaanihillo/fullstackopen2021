4266b614933d894ce9f4beaceacc9935f733c62e
banaanihillo <banaanihillo@protonmail.ch>
2021-07-11 15:03:39 +0300

    5-01: Added list of blogs

---
+++ exercise5/bloglist/src/components/Blog.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>
+    {{blog.title}} - by {{blog.author}} <br />
+    <a :href="blog.url"> {{blog.author}} </a>
+  </p>
+</template>
+
+<script>
+export default {
+  name: "Blog",
+  props: {
+    blog: Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
