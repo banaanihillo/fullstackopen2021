ca12b6bf4fdf47dd2c7c007865be5764cb500f70
banaanihillo <banaanihillo@protonmail.ch>
2021-07-11 15:04:17 +0300

    5-01: Added log-in form

---
+++ exercise5/bloglist/src/components/LogIn.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit.prevent="logIn">
+    <div class="input">
+      <label for="user-name-input"> User name </label>
+      <input
+        type="text"
+        v-model="credentials.userName"
+      />
+    </div>
+    <div class="input">
+      <label for="password-input"> Password </label>
+      <input
+        type="password"
+        v-model="credentials.password"
+      />
+    </div>
+    <button type="submit"> Log in </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      credentials: {
+        userName: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    logIn() {
+      this.$emit(
+        "log-in",
+        {...this.credentials}
+      )
+      this.credentials = {
+        userName: "",
+        password: ""
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
