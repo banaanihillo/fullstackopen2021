841109a6afba7f69139c2f92d35aa06b43939c1d
banaanihillo <banaanihillo@protonmail.ch>
2021-07-11 16:51:05 +0300

    5-03: Added Authorization header with token
    Added the currently logged-in user's token,
    to be sent with the post request,
    as the Authorization header

--- exercise5/bloglist/src/services/blogService.js
+++ exercise5/bloglist/src/services/blogService.js
@@ -1,11 +1,33 @@
 import axios from "axios"
 const baseURL = "http://localhost:3001/api/blogs"
 
+let _token = null
+
+const setToken = (token) => {
+  _token = `Bearer ${token}`
+}
+
 const getBlogs = async () => {
   const response = await axios.get(baseURL)
   return response.data
 }
 
+const addBlog = async (blogInput) => {
+  const configuration = {
+    headers: {
+      Authorization: _token
+    }
+  }
+  const response = await axios.post(
+    baseURL,
+    blogInput,
+    configuration
+  )
+  return response.data
+}
+
 export default {
-  getBlogs
+  getBlogs,
+  addBlog,
+  setToken
 }
