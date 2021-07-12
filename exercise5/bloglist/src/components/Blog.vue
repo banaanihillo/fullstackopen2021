4970cb01f5bbe56e9311740aa995fe3877176a9d
banaanihillo <banaanihillo@protonmail.ch>
2021-07-12 14:49:10 +0300

    5-07: Added expanded blog information
    Added a button for each blog,
    with which to display more information;
    changed the default display,
    into only showing the title and author

--- exercise5/bloglist/src/components/Blog.vue
+++ exercise5/bloglist/src/components/Blog.vue
@@ -1,8 +1,20 @@
 <template>
-  <p>
-    {{blog.title}} - by {{blog.author}} <br />
-    <a :href="blog.url"> {{blog.author}} </a>
-  </p>
+  <li v-if="expandedInformation">
+    <p> {{blog.title}} </p>
+    <p> {{blog.author}} </p>
+    <p>
+      {{blog.upvotes}} upvotes
+      <button @click="doNothing"> Upvote </button>
+    </p>
+    <p>
+      <a :href="blog.url"> {{blog.author}} </a>
+    </p>
+    <button @click="toggleExpandedInformation"> Collapse </button>
+  </li>
+  <li v-else>
+    {{blog.title}} - by {{blog.author}}
+    <button @click="toggleExpandedInformation"> Expand </button>
+  </li>
 </template>
 
 <script>
@@ -10,10 +22,30 @@ export default {
   name: "Blog",
   props: {
     blog: Object
+  },
+  data() {
+    return {
+      expandedInformation: false
+    }
+  },
+  methods: {
+    toggleExpandedInformation() {
+      this.expandedInformation = !this.expandedInformation
+    },
+    doNothing() {
+      
+    }
   }
 }
 </script>
 
 <style scoped>
+li {
+  border: 1px solid darkmagenta;
+  padding: 0.5em;
+}
 
+button {
+  margin-top: 0;
+}
 </style>
