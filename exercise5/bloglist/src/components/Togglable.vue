379a4dd7c2efb5c47855b953bc8ebe74c0ae245c
banaanihillo <banaanihillo@protonmail.ch>
2021-07-12 14:05:25 +0300

    5-05: Added an open/close modal
    Added a component with an open/close toggle,
    which itself is pretty simple,
    but with some additional bells and whistles,
    related to the emission of events both ways,
    due to the nature of the forms in this case;
    mainly, if the blog form was open on logout,
    the resulting login form needs to collapse as well,
    which is not really specified in the exercise,
    but seems like a logical choice here,
    at least in terms of perceived user experience

---
+++ exercise5/bloglist/src/components/Togglable.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <span v-if="formVisible">
+      <slot></slot>
+      <button @click="toggleVisibility">
+        Close
+      </button>
+    </span>
+    <span v-else>
+      <button @click="toggleVisibility">
+        {{buttonLabel}}
+      </button>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonLabel: String,
+    formVisible: Boolean
+  },
+  methods: {
+    toggleVisibility() {
+      // The parent component controls the visibility,
+      // since it needs access to the slots directly
+      // - this is probably simpler than passing the emission along,
+      // which would create a property passage two levels deep;
+      // this togglable element does not know about the forms,
+      // nor their individual state
+      this.$emit("toggle-visibility")
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
