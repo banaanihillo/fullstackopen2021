d720418116172069e85785b4a65a606ba47abeec
banaanihillo <banaanihillo@protonmail.ch>
2021-07-18 11:43:08 +0300

    Added lint configuration

--- exercise5/bloglist/.eslintrc.js
+++ exercise5/bloglist/.eslintrc.js
@@ -1,17 +1,29 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es6: true
   },
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended'
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    ecmaVersion: 2021,
+    sourceType: "module"
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-trailing-spaces": "error",
+    "arrow-spacing": [
+      "error",
+      {
+        before: true,
+        after: true
+      }
+    ]
   }
 }
