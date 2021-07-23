fc1553162e61c4522128da2bf677612e44b9ca00
banaanihillo <banaanihillo@protonmail.ch>
2021-07-23 19:07:37 +0300

    7-01: Removed unnecessary details
    Removed the filter component,
    as well as its associated computed getter

---
+++ exercise7/routed-anecdotes/src/views/AnecdoteList.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li v-for="anecdote in sortedAnecdotes" :key="anecdote.id">
+      <Anecdote :anecdote="anecdote" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import Anecdote from "../components/Anecdote.vue"
+
+export default {
+  components: {
+    Anecdote
+  },
+  computed: {
+    // filter not implemented
+    /* filteredSortedAnecdotes() {
+      return this.$store.getters.filteredSortedAnecdotes
+    } */
+    sortedAnecdotes() {
+      return this.$store.getters.sortedAnecdotes
+    }
+  },
+  created() {
+    this.$store.dispatch("initializeAnecdotes")
+  }
+}
+</script>
+
+<style>
+
+</style>
