c9b979b201f5610832624a4208db9a0461d318f1
banaanihillo <banaanihillo@protonmail.ch>
2021-07-23 19:03:35 +0300

    7-01: Added anecdotes from exercise 6

---
+++ exercise7/routed-anecdotes/src/services/anecdoteService.js
@@ -0,0 +1,26 @@
+import axios from "axios"
+const baseURL = "http://localhost:3000/anecdotes"
+
+const getAnecdotes = async () => {
+  const response = await axios.get(baseURL)
+  return response.data
+}
+
+const addAnecdote = async (newAnecdote) => {
+  const response = await axios.post(baseURL, newAnecdote)
+  return response.data
+}
+
+const updateAnecdote = async (anecdoteToUpdate) => {
+  const response = await axios.patch(
+    `${baseURL}/${anecdoteToUpdate.id}`,
+    anecdoteToUpdate
+  )
+  return response.data
+}
+
+export default {
+  getAnecdotes,
+  addAnecdote,
+  updateAnecdote
+}
