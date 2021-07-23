c3d96172e32adc5a775e037dc20c20fad104a828
banaanihillo <banaanihillo@protonmail.ch>
2021-07-23 20:45:57 +0300

    7-02: Added a page for individual anecdotes
    Modified how the anecdotes are shown,
    showing a list of links by default,
    and extended information on the new page,
    passing the anecdote ID as a route parameter,
    with which to find the corresponding anecdote

---
+++ exercise7/routed-anecdotes/src/views/Anecdote.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>
+      {{anecdote.content}}
+    </h2>
+    <p>
+      Votes: {{anecdote.votes}}
+    </p>
+    <button @click="addVote(anecdote)">
+      Vote
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    addVote(anecdote) {
+      this.$store.dispatch(
+        "addVote",
+        anecdote
+      )
+      // not implemented
+      /* this.$store.commit({
+        type: "SET_NOTIFICATION",
+        notification: `Successfully voted for ${anecdote.content}.`,
+        timeoutSeconds: 2
+      }) */
+    }
+  },
+  computed: {
+    anecdote() {
+      return this.$store.getters.sortedAnecdotes.find((anecdote) => {
+        return anecdote.id === this.$route.params.id
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
