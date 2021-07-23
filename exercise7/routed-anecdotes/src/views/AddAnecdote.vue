5fdb9b8a865d08c04b90059f1a9142380058064f
banaanihillo <banaanihillo@protonmail.ch>
2021-07-23 19:08:43 +0300

    7-01: Added routed anecdotes
    Changed the addition of anecdotes,
    as it no longer needs to empty its input on submit,
    instead pushing the user back to the anecdotes

---
+++ exercise7/routed-anecdotes/src/views/AddAnecdote.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent="addAnecdote">
+    <p>
+      <label for="anecdote-input">
+        Anecdote content
+      </label>
+      <br />
+      <input
+        id="anecdote-input"
+        v-model="anecdoteContent"
+        type="text"
+        required
+        minLength="1"
+      />
+    </p>
+    <button type="submit">
+      Submit
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      anecdoteContent: ""
+    }
+  },
+  methods: {
+    addAnecdote() {
+      this.$store.dispatch(
+        "addAnecdote",
+        {
+          content: this.anecdoteContent,
+          votes: 0
+        }
+      )
+      // not implemented
+      /* this.$store.commit({
+        type: "SET_NOTIFICATION",
+        notification: `Successfully added ${this.anecdoteContent}.`,
+        timeoutSeconds: 4
+      }) */
+      this.$router.push("/")
+    }
+  }
+}
+</script>
+
+<style scoped>
+input[type="text"] {
+  width: 30em;
+}
+</style>
