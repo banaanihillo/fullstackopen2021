4941c0d0b78acd6ae3c9798cf5e75bfa1c606049
banaanihillo <banaanihillo@protonmail.ch>
2021-07-23 21:35:55 +0300

    7-03: Added initialization of individual anecdote
    Added a Vuex initialization action,
    similar to that of the whole anecdote list,
    to prevent the anecdote from being undefined,
    if the anecdote page was reloaded or such,
    and moved the find by ID into Vuex,
    also re-added the vote notification

--- exercise7/routed-anecdotes/src/views/Anecdote.vue
+++ exercise7/routed-anecdotes/src/views/Anecdote.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+<span>
+  <div v-if="anecdote">
     <h2>
       {{anecdote.content}}
     </h2>
@@ -10,6 +11,12 @@
       Vote
     </button>
   </div>
+  <div v-else>
+    <p>
+      Loading
+    </p>
+  </div>
+</span>
 </template>
 
 <script>
@@ -20,20 +27,23 @@ export default {
         "addVote",
         anecdote
       )
-      // not implemented
-      /* this.$store.commit({
+      this.$store.commit({
         type: "SET_NOTIFICATION",
         notification: `Successfully voted for ${anecdote.content}.`,
         timeoutSeconds: 2
-      }) */
+      })
     }
   },
   computed: {
     anecdote() {
-      return this.$store.getters.sortedAnecdotes.find((anecdote) => {
-        return anecdote.id === this.$route.params.id
-      })
+      return this.$store.state.individualAnecdote
     }
+  },
+  created() {
+    this.$store.dispatch(
+      "getAnecdoteByID",
+      this.$route.params.id
+    )
   }
 }
 </script>
