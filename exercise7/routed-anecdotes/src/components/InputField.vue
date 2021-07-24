b3b76b57f718cd76531ee9e54d53e0307f305039
banaanihillo <banaanihillo@protonmail.ch>
2021-07-24 14:39:10 +0300

    7-04: Added a generic input field with label
    Added a component with a label and an input,
    taking the label and type as required properties,
    and optional minimum and maximum lengths,
    as well as a boolean for input requirement,
    handling the input value as local state,
    with emission back to the parent

---
+++ exercise7/routed-anecdotes/src/components/InputField.vue
@@ -0,0 +1,53 @@
+<template>
+  <p>
+    <label :for="`${label}-input`">
+      {{label}}
+    </label>
+    <br v-if="lineBreak" />
+    <input
+      :type="type"
+      :id="`${label}-input`"
+      :required="required"
+      :minLength="minLength"
+      :maxLength="maxLength"
+      :value="value"
+      @input="(event) => $emit('input', event.target.value)"
+    />
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    type: String,
+    required: {
+      type: Boolean,
+      required: false
+    },
+    minLength: {
+      type: String,
+      required: false
+    },
+    maxLength: {
+      type: String,
+      required: false
+    },
+    lineBreak: {
+      type: Boolean,
+      required: false
+    }
+  },
+  data() {
+    return {
+      value: ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+input[type="text"] {
+  width: 30em;
+}
+</style>
