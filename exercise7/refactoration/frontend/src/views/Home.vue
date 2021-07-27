2b5ab948e9d9150115f87e0f9747fb70defa58a6
banaanihillo <banaanihillo@protonmail.ch>
2021-07-27 18:04:29 +0300

    7-16: Moved most functionality out of Home
    Re-refactored the application component,
    into including most of the functionality again,
    including the currently logged-in user's information,
    as well as log-out functionality,
    which now also pushes the user back home,
    unless the user was already home

--- exercise7/refactoration/frontend/src/views/Home.vue
+++ exercise7/refactoration/frontend/src/views/Home.vue
@@ -1,23 +1,5 @@
 <template>
-<span>
-  <span v-if="!loggedIn">
-    <h2> Log in </h2>
-    <Togglable
-      buttonLabel="Log in"
-      :formVisible="formVisible"
-      @toggle-visibility="toggleVisibility"
-    >
-      <LogIn @toggle-visibility="toggleVisibility" />
-    </Togglable>
-  </span>
-  <span v-else>
-    <p>
-      Logged in as {{loggedIn.userName}}. <br />
-      <button @click="logOut">
-        Log out
-      </button>
-    </p>
-
+  <span v-if="loggedIn">
     <h2> Add blog </h2>
     <Togglable
       buttonLabel="Add blog"
@@ -26,57 +8,29 @@
     >
       <AddBlog @toggle-visibility="toggleVisibility" />
     </Togglable>
-
-    <h2> List of blogs </h2>
-    <Blogs />
-
-    <h2> Table of users </h2>
-    <Users />
-  </span>
-</span>
+  </span> <!---->
 </template>
 
-<script>
-import Blogs from "../components/Blogs.vue"
-import LogIn from "../components/LogIn.vue"
+<script> //
 import AddBlog from "../components/AddBlog.vue"
-import Togglable from "../components/Togglable.vue"
-import Users from "../components/Users.vue"
+import Togglable from "../components/Togglable.vue" //
 
 export default {
-  components: {
-    Blogs,
-    LogIn,
+  components: { //
     AddBlog,
-    Togglable,
-    Users
+    Togglable
   },
   data() {
     return {
       formVisible: false
     }
   },
-  computed: { //
+  computed: {
     loggedIn() {
       return this.$store.state.loggedIn
     }
   },
-  async created() {
-    this.$store.dispatch("initializeBlogs")
-    this.$store.dispatch("initializeUsers")
-  },
-  methods: {
-    logOut() {
-      this.$store.commit("LOG_OUT")
-      this.$store.commit(
-        "SET_NOTIFICATION",
-        {
-          notification: "Logged out successfully.",
-          timeoutDuration: 3000
-        }
-      )
-      this.formVisible = false
-    },
+  methods: { //
     toggleVisibility() {
       this.formVisible = !this.formVisible
     }
