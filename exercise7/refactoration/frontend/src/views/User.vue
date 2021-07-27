bdc18a976e0193c799eaec58af8ef16acc2acaa9
banaanihillo <banaanihillo@protonmail.ch>
2021-07-27 14:54:14 +0300

    7-14: Added individual user information
    Added information on the blogs added by a user,
    using the new individual user state,
    to display the blog titles and authors,
    also added a link back home

---
+++ exercise7/refactoration/frontend/src/views/User.vue
@@ -0,0 +1,43 @@
+<template>
+<span v-if="individualUser">
+  <h2> {{individualUser.userName}} </h2>
+  <h3> Blogs added </h3>
+
+  <span v-if="individualUser.blogs.length === 0">
+    <p>
+      No blogs added.
+    </p>
+  </span>
+  <span v-else>
+    <ul>
+      <li v-for="blog in individualUser.blogs" :key="blog.id">
+        {{blog.title}} - by {{blog.author}}
+      </li>
+    </ul>
+  </span>
+
+  <router-link to="/">
+    Back to home page
+  </router-link>
+</span>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$store.dispatch(
+      "initializeIndividualUser",
+      this.$route.params.id
+    )
+  },
+  computed: {
+    individualUser() {
+      return this.$store.state.individualUser
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
